758bcc5aeaa877f5cc3320a1e8e7166cdea1b570
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-21 20:24:01 -0300

    C2 - Finaliza exercicio 1, Adiciona arquivo exercicio 2

--- fundamentos/4-introducao-a-javascript/exercicios.01/exercicio1.js
+++ fundamentos/4-introducao-a-javascript/exercicios.01/exercicio1.js
@@ -1,8 +1,11 @@
 const myName = 'Stefano';
 const birthCity = 'Belo Horizonte';
+birthCity = 'Vespasiano';
 let birthYear = 1992;
+birthYear = 2030;
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
 
 
