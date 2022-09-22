3f44262a6acb6b132afea872f7495c32c3df18f1
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-22 11:57:39 -0300

    C6 - Adiciona arquivo true-exercicio.js  para começar o bloco de exercicios do dia

--- fundamentos/4-introducao-a-javascript/exercicios.01/exercicio2.js
+++ fundamentos/4-introducao-a-javascript/exercicios.01/exercicio2.js
@@ -17,7 +17,6 @@ const height = 8;
 const area = 5 * 8;
 const perimeter = 5 + 8;
 console.log(area);
-console.log(perimeter);
-
+console.log(perimeter); 
 
 
--- fundamentos/4-introducao-a-javascript/exercicios.01/exercicio3.js
+++ fundamentos/4-introducao-a-javascript/exercicios.01/exercicio3.js
@@ -4,9 +4,9 @@
 const nota = 50;
 
 if (nota >= 80) {
-  console.log("Parabéns, você foi aprovado!");
+  console.log("Parabéns, você foi aprovado!")
 } else if (nota < 80 && nota >= 60) {
-  console.log("Você está na nossa lista de espera");
+  console.log("Você está na nossa lista de espera")
 } else {
   console.log("Você foi reprovada(o)")
 }
