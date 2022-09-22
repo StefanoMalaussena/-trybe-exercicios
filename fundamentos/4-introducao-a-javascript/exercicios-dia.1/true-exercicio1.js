b8393bf5e04e58671681371cbe30794b1a76cac8
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-22 20:05:49 -0300

    C4 - conclui exercicios até o do tringulo

---
+++ fundamentos/4-introducao-a-javascript/exercicios-dia.1/true-exercicio1.js
@@ -0,0 +1,50 @@
+// const a = 2;
+// const b = 7;
+// const c = 12;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// if (a > b) {
+//     console.log('a é maior')
+// }
+// else {
+//     console.log('b é maior')
+// }
+
+// if (a > b && a > c) {
+//     console.log('a é maior')
+// }
+// else if (b > a && b > c) {
+//     console.log('b é maior')
+// }
+// else {
+//     console.log('c é maior')
+// }
+
+// if (a > 0) {
+//     console.log('positive')
+// } 
+// else if (a === 0) {
+//     console.log('zero')
+// }
+// else {
+//     console.log('negative')
+// }
+
+const a1 = 60;
+const a2 = 60;
+const a3 = 60;
+
+if (a1 < 180 && a1 > 1 && a2 < 180 && a2 > 1 && a3 < 180 && a3 > 1) {
+    console.log(true)
+} 
+else {
+    console.log(false)
+}
+
+
