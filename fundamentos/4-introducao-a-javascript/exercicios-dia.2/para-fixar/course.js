1936b1fcf07359e5c6d0e1889d9727ac16ac3057
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-22 16:15:05 -0300

    C2 - Adiciona exemplos de estruturas de repetiçao (for)

--- fundamentos/4-introducao-a-javascript/exercicios-dia.2/para-fixar/course.js
+++ fundamentos/4-introducao-a-javascript/exercicios-dia.2/para-fixar/course.js
@@ -1,13 +1,31 @@
-let numero = 7;
 
-console.log(numero * 1)
-console.log(numero * 2)
-console.log(numero * 3)
-console.log(numero * 4)
-console.log(numero * 5)
-console.log(numero * 6)
-console.log(numero * 7)
-console.log(numero * 8)
-console.log(numero * 9)
+
+// console.log(numero * 1)
+// console.log(numero * 2)
+// console.log(numero * 3)
+// console.log(numero * 4)
+// console.log(numero * 5)
+// console.log(numero * 6)
+// console.log(numero * 7)
+// console.log(numero * 8)
+// console.log(numero * 9)
+
+// let numero = 7;
+
+
+// for (let contador = 1; contador <= 9; contador ++) {
+//     console.log(numero * contador);
+//     console.log('');
+// }
+
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+
+for (let index = 0; index < listaDeNomes.length; index++) {
+    let mensagem = 'Boas vindas, ' + listaDeNomes[index] + '!';
+    console.log(mensagem)
+}
+
+
+
 
  
