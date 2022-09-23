c71ca3d059c78200b047e0f1e82b016ca7ff44c8
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-22 22:27:24 -0300

    C5 - Adiciona e resolve exercicio porcentagem retornar como letra

--- fundamentos/4-introducao-a-javascript/exercicios-dia.1/true-exercicio1.js
+++ fundamentos/4-introducao-a-javascript/exercicios-dia.1/true-exercicio1.js
@@ -36,15 +36,60 @@
 //     console.log('negative')
 // }
 
-const a1 = 60;
-const a2 = 60;
-const a3 = 60;
+// const a1 = 60;
+// const a2 = 60;
+// const a3 = 60;
 
-if (a1 < 180 && a1 > 1 && a2 < 180 && a2 > 1 && a3 < 180 && a3 > 1) {
-    console.log(true)
-} 
-else {
-    console.log(false)
+// if (a1 < 180 && a1 > 1 && a2 < 180 && a2 > 1 && a3 < 180 && a3 > 1) {
+//     console.log(true)
+// } 
+// else {
+//     console.log(false)
+// }
+
+// const a = 90;
+// const b = 80;
+// const c = 70;
+// const d = 60;
+// const e = 50;
+// const f = 49;
+
+// * Utilize if/else para fazer um programa que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+//     * Porcentagem >= 90 -> A 
+//     * Porcentagem >= 80 -> B 
+//     * Porcentagem >= 70 -> C 
+//     * Porcentagem >= 60 -> D 
+//     * Porcentagem >= 50 -> E 
+//     * Porcentagem < 50 -> F 
+//     * O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. 
+
+
+
+const nota1 = 120
+
+if (nota1 >= 90 && nota1 <= 100) {
+    console.log('A')
 }
+else if (nota1 >= 80 && nota1 <= 89) {
+    console.log('B')
+}
+else if (nota1 >= 70 && nota1 <= 79) {
+    console.log('C')
+}
+else if (nota1 >= 60 && nota1 <= 69) {
+    console.log('D')
+}
+else if (nota1 >= 50 && nota1 <= 59) {
+    console.log('E')
+}
+else if (nota1 > 0 && nota1 < 50) {
+    console.log('F')
+}
+else {
+    console.log('error')
+}
+
+
+
 
 
