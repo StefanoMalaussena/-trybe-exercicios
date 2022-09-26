c00813f91a16ad4536e666fea8ce5e8e95c34092
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-26 20:08:45 -0300

    C3 - Adiciona exercicio da explicaçao das functions e resolucao dos 2 primeiros topicos

--- fundamentos/4-introducao-a-javascript/exercicios-dia.4/parafixar.js
+++ fundamentos/4-introducao-a-javascript/exercicios-dia.4/parafixar.js
@@ -33,26 +33,77 @@
 
 // Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
-  };
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
 
-  for ( let key in names) {
-    console.log('Olá ' + names[key]);
-  };
+//   for ( let key in names) {
+//     console.log('Olá ' + names[key]);
+//   };
 
-//   Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+// //   Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
 
-let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
 
-  for (let key in car) {
-    console.log(key, car[key]);
-  };
+//   for (let key in car) {
+//     console.log(key, car[key]);
+//   };
 
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+// Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+// Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+// Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+// Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo.
   
+function soma (som1, som2) {
+  return (som1 + som2)
+};
+
+function subtracao (sub1, sub2) {
+  return (sub1 - sub2)
+};
+
+function multiplicacao (mult1, mult2) {
+  return (mult1 * mult2)
+};
+
+function divisao (div1, div2) {
+  return (div1 / div2)
+};
+
+function modulo (mod1, mod2) {
+  return (mod1 % mod2)
+}
+
+
+// console.log(soma (20, 3));
+// console.log(subtracao(20, 3));
+// console.log(multiplicacao (20, 3));
+// console.log(divisao (20, 3));
+// console.log(modulo (20, 3));
+
+function maiorNumero (valor1, valor2) {
+  if (valor1 > valor2) {
+  return (valor1 + ' é o maior número.')
+  }
+  else if (valor1 === valor2) {
+  return ('Os números sao iguais.')  
+  }
+  else {
+    return (valor2 + ' é o maior número.')
+  }
+}
+console.log(maiorNumero(34, 23));
