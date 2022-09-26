b7ee5d4f634ca965d101f7c8ed7124363479ae03
StefanoMalaussena <malaussenapsn@gmail.com>
2022-09-26 15:21:25 -0300

    C1 - Adiciona diretorio exercicios-dia.4, adiciona arquivo parafixar.js

---
+++ fundamentos/4-introducao-a-javascript/exercicios-dia.4/parafixar.js
@@ -0,0 +1,19 @@
+// let diasDaSemana = {
+//   1: 'domingo',
+//   2: 'segunda',
+//   3: 'terça',
+//   4: 'quarta',
+//   5: 'quinta',
+//   6: 'sexta',
+//   7: 'sábado',
+// };
+
+// diasDaSemana.1; // SyntaxError: Unexpected number
+// console.log(diasDaSemana['1']); // domingo
+
+// Para fixar
+// Vamos fazer alguns exercícios de fixação para consolidar os conhecimentos adquiridos! 
+
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+
+
